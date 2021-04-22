c7e443624de25ef78d85db9419630d1e5f572513
Akshata Deshpande <ad.march6@gmail.com>
2021-04-22 10:29:15 +0100

    Mispelled comitijfods mesagae

--- app/src/main/java/com/example/rxjavaplaystation/schedulers/RxObserveOnActivity.kt
+++ app/src/main/java/com/example/rxjavaplaystation/schedulers/RxObserveOnActivity.kt
@@ -1,4 +1,21 @@
 package com.example.rxjavaplaystation.schedulers
 
-class RxObserveOnActivity {
+import android.os.Bundle
+import com.example.rxjavaplaystation.BaseActivity
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.schedulers.Schedulers
+import timber.log.Timber
+
+class RxObserveOnActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Observable.just("Apple","Orange","Banana")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe{ Timber.d("Received $it on thread ${Thread.currentThread().name}")}    djfnkdjfn
+    }
 }
